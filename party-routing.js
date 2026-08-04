d6405f95108cc53e8808289784b2e159381cce09
memetreza34-ux <memetreza34@gmail.com>
2026-08-04 17:33:00 +0200

    Route advanced party games through a focused standalone play screen

---
+++ party-routing.js
@@ -0,0 +1,33 @@
+(function (root, factory) {
+  const base = typeof module === 'object' && module.exports
+    ? require('./party-expansion.js')
+    : root.SecretCirclePartyCatalog;
+  const api = factory(base);
+  if (typeof module === 'object' && module.exports) module.exports = api;
+  else root.SecretCirclePartyCatalog = api;
+})(typeof globalThis !== 'undefined' ? globalThis : this, function (base) {
+  'use strict';
+  if (!base) throw new Error('Erweiterter Party-Katalog fehlt.');
+
+  const advancedModes = new Set(['two-truths', 'question-imposter', 'location-spy', 'mafia']);
+  const games = base.games.map(game => advancedModes.has(game.mode)
+    ? Object.freeze({
+        ...game,
+        advancedMode: game.mode,
+        mode: 'link',
+        href: `advanced.html?game=${encodeURIComponent(game.id)}`
+      })
+    : game
+  );
+
+  function getGame(id) {
+    return games.find(game => game.id === id) || null;
+  }
+
+  return Object.freeze({
+    ...base,
+    version: 3,
+    games: Object.freeze(games),
+    getGame
+  });
+});
