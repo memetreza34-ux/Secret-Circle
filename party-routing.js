7330025526305e06361239f015a28968682b1e4a
memetreza34-ux <memetreza34@gmail.com>
2026-08-05 18:05:53 +0200

    Route advanced games after loading the ten new quick modes

--- party-routing.js
+++ party-routing.js
@@ -1,6 +1,6 @@
 (function (root, factory) {
   const base = typeof module === 'object' && module.exports
-    ? require('./party-expansion.js')
+    ? require('./party-trending-catalog.js')
     : root.SecretCirclePartyCatalog;
   const api = factory(base);
   if (typeof module === 'object' && module.exports) module.exports = api;
@@ -26,7 +26,7 @@
 
   return Object.freeze({
     ...base,
-    version: 3,
+    version: 4,
     games: Object.freeze(games),
     getGame
   });
