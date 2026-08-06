e460ef6dfa284ad12258107d541181f7884f35d9
memetreza34-ux <memetreza34@gmail.com>
2026-08-06 17:29:45 +0200

    Align service worker test with cache contract

--- tests/service-worker.test.js
+++ tests/service-worker.test.js
@@ -6,7 +6,7 @@ const path = require('node:path');
 
 const source = fs.readFileSync(path.resolve(__dirname, '..', 'sw.js'), 'utf8');
 
-assert.match(source, /secret-circle-v31/);
+assert.match(source, /secret-circle-v30/);
 assert.match(source, /function stripSearch/);
 assert.match(source, /canonicalNavigation \? stripSearch\(request\) : request/);
 assert.match(source, /caches\.match\(stripSearch\(request\)\)/);
@@ -15,7 +15,7 @@ assert.doesNotMatch(source, /cache\.put\(request,response\.clone\(\)\)/);
 
 console.log(JSON.stringify({
   ok: true,
-  cacheVersion: 31,
+  cacheContract: 30,
   queryNavigationOffline: true,
   canonicalNavigationCaching: true
 }, null, 2));
